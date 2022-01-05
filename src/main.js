eaf6b3bc1e46e3a9da18920b1cb9874f22a9afed
alkaidchen <362774405@qq.com>
2022-01-05 14:00:14 +0800

    2022/01/05 14:00

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import TDesign from 'tdesign-vue';
+// 引入组件库全局样式资源
+import 'tdesign-vue/es/style/index.css';
+Vue.use(TDesign);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
