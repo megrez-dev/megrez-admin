341581f1c370177e6369d43d02941008d24848ac
alkaidchen <362774405@qq.com>
2022-06-03 17:25:29 +0800

    feat:new journal dialog

--- src/views/journal/components/NewJournalDialog.vue
+++ src/views/journal/components/NewJournalDialog.vue
@@ -17,26 +17,36 @@
     <div class="uploaded-files-container">
       <div
         class="uploaded-files-item"
-        v-for="image in images"
+        v-for="(image, index) in images"
         :key="image.name"
+        @mouseenter="mouseEnter(image, index)"
+        @mouseleave="mouseLeave(image, index)"
       >
         <img :src="image.url" />
+        <transition name="fade" mode="out-in">
+          <div class="uploaded-files-mask" v-show="items[index]">
+            <BrowseIcon style="cursor: pointer" @click="handlePreview(image)" />
+            <t-divider layout="vertical" />
+            <DeleteIcon style="cursor: pointer" @click="handleDelete(index)" />
+          </div>
+        </transition>
       </div>
     </div>
     <t-upload
-      refs="upload"
       action="https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo"
       v-model="images"
       theme="custom"
       tips="最多上传 9 张图片"
       accept="image/*"
       multiple
+      :onSuccess="uploadSuccess"
       :max="9"
     ></t-upload>
   </t-dialog>
 </template>
 
 <script>
+import { BrowseIcon, DeleteIcon } from "tdesign-icons-vue";
 export default {
   name: "NewJournalDialog",
   data() {
@@ -46,6 +56,7 @@ export default {
         content: "",
       },
       images: [],
+      items: [],
     };
   },
   methods: {
@@ -57,13 +68,32 @@ export default {
     },
     publishJournal() {
       this.$message.info("未实现");
-      console.log(this.$refs.upload);
     },
-    handlePreview({ file }) {
-      this.$viewerApi({
-        images: [file.url],
+    mouseEnter(image, index) {
+      this.$set(this.items, index, true);
+    },
+    mouseLeave(image, index) {
+      this.$set(this.items, index, false);
+    },
+    uploadSuccess() {
+      this.items = this.images.map(() => {
+        return false;
       });
     },
+    handlePreview(image) {
+      this.$viewerApi({
+        images: [image.url],
+      });
+    },
+    handleDelete(index) {
+      console.log(index);
+      this.images.splice(index, 1);
+      this.items.splice(index, 1);
+    },
+  },
+  components: {
+    BrowseIcon,
+    DeleteIcon,
   },
 };
 </script>
@@ -72,19 +102,54 @@ export default {
 @import "@/style/variables";
 .uploaded-files-container {
   display: flex;
+  flex-wrap: wrap;
   .uploaded-files-item {
-    width: 130px;
-    height: 130px;
-    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 128px;
+    height: 128px;
     margin: 0 10px 10px 0;
     border: 1px solid @border-level-2-color;
-    &:hover {
-      background-color: @text-color-secondary;
-    }
     img {
       width: 100%;
       margin: auto;
     }
+    .uploaded-files-mask {
+      background-color: @text-color-secondary;
+      color: @bg-color-container;
+      transition: opacity 0.5s ease-in-out;
+      will-change: transform;
+      opacity: 100;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+    }
   }
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 0.5s;
+}
+.fade-enter-to {
+  opcaity: 0;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: all 2s;
+}
+.fade-leave-to {
+  opcaity: 0;
 }
 </style>
