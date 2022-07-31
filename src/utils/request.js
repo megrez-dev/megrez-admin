f53dc134c04ac3f9f37f28a014fe2d98ac99787e
marstonchen <marstonchen@tencent.com>
2022-07-31 17:55:28 +0800

    fix(*): 修复登录注册时两次请求问题，优化全局请求异常捕获

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,71 +1,69 @@
-import Vue from 'vue'
-import axios from 'axios';
-import host from '@/config/host';
-import router from '@/router'
-import { ACCESS_TOKEN } from '@/store/mutation-types'
+import Vue from "vue";
+import axios from "axios";
+import host from "@/config/host";
+import router from "@/router";
+import { ACCESS_TOKEN } from "@/store/mutation-types";
 // import store from '@/store'
-import { MessagePlugin } from 'tdesign-vue';
+import { MessagePlugin } from "tdesign-vue";
 
 // const env = import.meta.env.MODE || 'development';
 
 // const API_HOST = env === 'mock' ? '/' : host[env].API; // 如果是mock模式 就不配置host 会走本地Mock拦截
-const API_HOST = host['dev'].API
+const API_HOST = host["dev"].API;
 
 const CODE = {
-    SUCCESS: 0,
-    ERROR: -1,
-    NOT_INSTALL: 2,
+  SUCCESS: 0,
+  ERROR: -1,
+  NOT_INSTALL: 2,
 
-    TOKEN_NOT_EXIST: 1004,
-    TOKEN_INVALID: 1005,
-    TOKEN_EXPIRED: 1006,
-
+  TOKEN_NOT_EXIST: 1004,
+  TOKEN_INVALID: 1005,
+  TOKEN_EXPIRED: 1006,
 };
 
 const instance = axios.create({
-    baseURL: API_HOST,
-    timeout: 10000,
-    withCredentials: false,
+  baseURL: API_HOST,
+  timeout: 10000,
+  withCredentials: false,
 });
 
 instance.interceptors.request.use(function (config) {
-    let token = Vue.ls.get(ACCESS_TOKEN)
-    config.headers.Authorization = token;
-    return config;
+  let token = Vue.ls.get(ACCESS_TOKEN);
+  config.headers.Authorization = token;
+  return config;
 });
 
 instance.interceptors.response.use(
-    (response) => {
-        if (response.status === 200) {
-            if (response.data.status === CODE.SUCCESS) {
-                return response.data;
-            } else if (response.data.status === CODE.NOT_INSTALL) {
-                MessagePlugin.warning('请先安装博客');
-                router.push({ name: 'Install' });
-                return Promise.reject(response);
-            } else if (response.data.status === CODE.TOKEN_NOT_EXIST || response.data.status === CODE.TOKEN_INVALID || response.data.status === CODE.TOKEN_EXPIRED) {
-                MessagePlugin.warning('请先登录');
-                router.push({ name: 'Login' });
-                return Promise.reject(response);
-                // TODO: CODE.TOKEN_INVALID, TOKEN_EXPIRED 弹出登录框重新校验
-            } else {
-                if (response.data && response.data.msg) {
-                    MessagePlugin.warning(response.data.msg)
-                } else {
-                    MessagePlugin.warning('请求错误')
-                }
-                return Promise.reject(response);
-            }
-        } else {
-            MessagePlugin.error('网络请求失败');
-            return Promise.reject(response);
-        }
-    },
-    (err) => {
-        MessagePlugin.error('网络请求失败')
-        console.log(err)
-        return Promise.reject(err);
-    },
+  (response) => {
+    if (response.status !== 200) return Promise.reject(response);
+    const { status, msg } = response.data;
+    if (status === CODE.SUCCESS) return response.data;
+    // 错误处理
+    switch (status) {
+      // 未安装
+      case CODE.NOT_INSTALL: {
+        router.push({ name: "Install" });
+        break;
+      }
+      // 未登录，身份过期等
+      case CODE.TOKEN_EXPIRED:
+      case CODE.TOKEN_INVALID:
+      case CODE.TOKEN_NOT_EXIST: {
+        router.push({ name: "Login" });
+        break;
+      }
+    }
+    if (msg) {
+      MessagePlugin.warning(msg);
+    } else {
+      MessagePlugin.warning('未知服务端错误');
+    }
+    return Promise.reject(response);
+  },
+  (err) => {
+    MessagePlugin.warning('请求路径或请求参数错误');
+    return Promise.reject(err);
+  }
 );
 
 export default instance;
